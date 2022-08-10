2d8d3da9dd2e2a62361c94d5cb93cb125b23ba94
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-10 15:18:33 +0100

    added class Phone

--- phones/models.py
+++ phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.PositiveIntegerField()
+    name = models.CharField(max_length=250)
+    price = models.PositiveIntegerField()
+    image = models.URLField(max_length=200)
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=200, populate_from='name', unique=True, null=True)
