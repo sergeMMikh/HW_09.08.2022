d719ef7a0eb5f77b63b73d8252ad6a3c9cafbc59
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-11 10:04:28 +0100

    added import_phones

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -3,6 +3,8 @@ import csv
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
+from django.utils.text import slugify
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -13,5 +15,10 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            ph = Phone.object.create(id=phone.get('id'),
+                                     name=phone.get('name'),
+                                     price=phone.get('price'),
+                                     image=phone.get('image'),
+                                     release_date=phone.get('release_date'),
+                                     lte_exists=phone.get('lte_exists'),
+                                     slug=slugify(phone.get('name')))
--- phones/models.py
+++ phones/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    id = models.PositiveIntegerField()
+    id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=250)
     price = models.PositiveIntegerField()
     image = models.URLField(max_length=200)
     release_date = models.DateField(auto_now=False, auto_now_add=False)
     lte_exists = models.BooleanField()
-    slug = models.SlugField(max_length=200, populate_from='name', unique=True, null=True)
+    slug = models.SlugField(max_length=200)
