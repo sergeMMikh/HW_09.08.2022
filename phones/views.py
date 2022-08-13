49be9bac720c150e0d0d037c3800f14145cb9362
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-13 16:06:13 +0100

    added sort by

--- phones/views.py
+++ phones/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render, redirect
 from phones.models import Phone
-from pprint import pprint
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort', "no")
+
+    match sort:
+        case 'no':
+            phones = Phone.objects.all()
+        case 'name':
+            phones = Phone.objects.all().order_by('name')
+        case 'min_price':
+            phones = Phone.objects.all().order_by('price')
+        case 'max_price':
+            phones = Phone.objects.all().order_by('price').reverse()
+        case _:
+            phones = Phone.objects.all()
+
     template = 'catalog.html'
-    phones = Phone.objects.all()
+
     context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phones = Phone.objects.filter(slug=slug)
+    phones = Phone.objects.get(slug=slug)
     context = {'phone': phones}
     return render(request, template, context)
