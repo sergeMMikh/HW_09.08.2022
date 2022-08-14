0e778964f41e7a6717f53729710937c8571077f4
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-14 10:06:56 +0100

    added Django administration

--- phones/admin.py
+++ phones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 from .models import Phone
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'price', 'release_date', 'lte_exists'
+    list_filter = 'name', 'price'
